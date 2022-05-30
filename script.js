7e0f4fccee38e413715e5a815f57a8fcb42a7e68
TayTufenkjian <taytuf@gmail.com>
2022-05-30 15:39:13 -0400

    Create and display a 3x3 grid for the game board

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // do stuff
+
+    // Create and display an empty 3x3 grid
+    const gameBoard = (() => {
+        let grid = [
+                ['','',''], 
+                ['','',''], 
+                ['','',''], 
+            ];
+
+        let boardDiv = document.getElementById('game-board');
+
+        for (row of grid) {
+            for (col of row) {
+                let square = document.createElement('div');
+                square.className = 'grid-square'
+                boardDiv.append(square);
+            }
+        };      
+    })();
+    
 })
