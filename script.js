aff5eb599b310b192f75dc2cdbc9280e67436f79
TayTufenkjian <taytuf@gmail.com>
2022-05-30 16:01:29 -0400

    Add game module with alternating X/O clicks

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Create and display an empty 3x3 grid
-    const gameBoard = (() => {
+    // Module to create and display an empty 3x3 grid
+    const GameBoard = (() => {
         let grid = [
                 ['','',''], 
                 ['','',''], 
@@ -18,5 +18,33 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         };      
     })();
-    
-})
+
+    // Factory function to create players
+    const Player = (name, playSymbol) => {
+        return {name, playSymbol};
+    };
+
+    // Module to handle game play
+    const Game = (() => {
+        const player1 = Player('player1', 'X');
+        const player2 = Player('player2', 'O');
+
+        // Player 1 goes first
+        let currentPlayer = player1;
+
+        // Listen for click on each grid square
+        let squares = document.querySelectorAll('.grid-square');
+        squares.forEach(square => square.addEventListener('click', (event) => {
+                let targetSquare = event.target;
+                if (currentPlayer === player1) {
+                    targetSquare.textContent = player1.playSymbol;
+                    currentPlayer = player2;
+                } else {
+                    targetSquare.textContent = player2.playSymbol;
+                    currentPlayer = player1;
+                }
+            })
+        )
+    })();
+
+});
