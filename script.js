1225e68c1ddb9c2746437d11e425992176ab02df
TayTufenkjian <taytuf@gmail.com>
2022-05-31 16:21:22 -0400

    Click the button to start/restart a game
    Previously the game play would start on page load,
    and you'd have to reload the page to play a new game

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Module to create and display a 3x3 grid
-    const GameBoard = (() => {
-        let grid = [
+    // Create the initial game board
+    GameBoard.createGrid();
+    
+    // Listen for a click on the Start New Game button
+    let startButton = document.getElementById('start');
+    startButton.addEventListener('click', () => {
+        document.getElementById('game-result').textContent = '';
+        Game();
+    })
+});
+
+
+// Module to create, display, and update the board grid
+const GameBoard = (() => {
+    let grid;
+
+    // Create a clean grid
+    function createGrid() {     
+        grid = [
                 ['','',''], 
                 ['','',''], 
                 ['','',''], 
             ];
 
         let boardDiv = document.getElementById('game-board');
+        boardDiv.innerHTML = '';
 
         let rowIndex = 0;     
         for (col of grid) {
@@ -22,160 +39,158 @@ document.addEventListener('DOMContentLoaded', () => {
                 colIndex += 1;
             }
             rowIndex += 1;
-        };      
+        };     
+    } 
 
-        // Function for storing a symbol in the grid
-        function addToGrid(char, row, col) {
-            grid[row][col] = char;
-        }
+    // Store a symbol in one cell of the grid
+    function addToGrid(char, row, col) {
+        grid[row][col] = char;
+    }
 
-        // Function for getting a value from the grid
-        function getFromGrid(i, j) {
-            return grid[i][j];
-        }
+    // Get the value from a cell in the grid
+    function getFromGrid(i, j) {
+        return grid[i][j];
+    }
 
-        return {addToGrid, getFromGrid};
-    })();
-
-
-    // Factory function to create players
-    const Player = (name, playSymbol) => {
-        return {name, playSymbol};
-    };
+    return {addToGrid, getFromGrid, createGrid};
+})();
 
 
-    // Module to handle game play
-    const Game = (() => {
-        const player1 = Player('player1', 'X');
-        const player2 = Player('player2', 'O');
+// Factory function to create players
+const Player = (name, playSymbol) => {
+    return {name, playSymbol};
+};
 
-        // Player 1 goes first
-        let currentPlayer = player1;
 
-        // Initialize the count of plays
-        let playCounter = 0;
+ // Module to handle game play
+ const Game = () => {
+    // Create a clean grid
+    GameBoard.createGrid();
 
-        // Function to display game result
-        function showGameResult(winner) {
-            let resultDiv = document.getElementById('game-result');
-            resultDiv.textContent = (winner === 'tie' ? 'It\'s a tie' : `${winner.name} wins!`);
-        }
+    // Clear any previous game result
+    document.getElementById('game-result').textContent = '';
 
-        // Listen for click on each grid square
-        let squares = document.querySelectorAll('.grid-square');
-        squares.forEach(square => square.addEventListener('click', (event) => {
+    // Create players
+    const player1 = Player('player1', 'X');
+    const player2 = Player('player2', 'O');
 
-                // Get the square that was clicked
-                let targetSquare = event.target;
+    // Player 1 goes first
+    let currentPlayer = player1;
 
-                // Make sure that square is empty
-                if (targetSquare.textContent === '') {
+    // Initialize the count of plays
+    let playCounter = 0;
 
-                    // Mark the square with the player's symbol
-                    // Add the player's symbol to the board array
-                    if (currentPlayer === player1) {
-                        targetSquare.textContent = player1.playSymbol;
-                        targetSquare.classList.add('player1');
-                        currentPlayer = player2;
-                    } else {
-                        targetSquare.textContent = player2.playSymbol;
-                        targetSquare.classList.add('player2');
-                        currentPlayer = player1;
-                    }
-                    GameBoard.addToGrid(targetSquare.textContent, targetSquare.dataset.row, targetSquare.dataset.col);
-                    playCounter += 1;
+    // Function to display game result
+    function showGameResult(winner) {
+        let resultDiv = document.getElementById('game-result');
+        resultDiv.textContent = (winner === 'tie' ? 'It\'s a tie' : `${winner.name} wins!`);
+    }
 
-                    // After the fifth play, there are three Xs on the board
-                    // so at that point we should check for a win
-                    if (playCounter >= 5) {
+    // Listen for click on each grid square
+    let squares = document.querySelectorAll('.grid-square');
+    squares.forEach(square => square.addEventListener('click', (event) => {
 
-                        let winner;
+            // Get the square that was clicked
+            let targetSquare = event.target;
 
-                        // Check rows
-                       for (let i = 0; i < 3; i++) {
-                           let rowString = '';
-                           for (let j = 0; j < 3; j++) {
-                               switch (GameBoard.getFromGrid(i, j)) {
-                                   case 'X':
-                                       rowString += 'X';
-                                       break;
-                                    case 'O':
-                                        rowString += 'O';
-                                        break;
-                                    default:
-                                        break;
-                               }
-                           }
-                           switch (rowString) {
-                               case 'XXX':
-                                   winner = player1;
-                                   break;
-                                case 'OOO':
-                                    winner = player2;
+            // Make sure that square is empty
+            if (targetSquare.textContent === '') {
+
+                // Mark the square with the player's symbol
+                // Add the player's symbol to the board array
+                if (currentPlayer === player1) {
+                    targetSquare.textContent = player1.playSymbol;
+                    targetSquare.classList.add('player1');
+                    currentPlayer = player2;
+                } else {
+                    targetSquare.textContent = player2.playSymbol;
+                    targetSquare.classList.add('player2');
+                    currentPlayer = player1;
+                }
+                GameBoard.addToGrid(targetSquare.textContent, targetSquare.dataset.row, targetSquare.dataset.col);
+                playCounter += 1;
+
+                // After the fifth play, there are three Xs on the board
+                // so at that point we should check for a win
+                if (playCounter >= 5) {
+
+                    let winner;
+
+                    // Check rows
+                for (let i = 0; i < 3; i++) {
+                    let rowString = '';
+                    for (let j = 0; j < 3; j++) {
+                        switch (GameBoard.getFromGrid(i, j)) {
+                            case 'X':
+                                rowString += 'X';
+                                break;
+                                case 'O':
+                                    rowString += 'O';
                                     break;
                                 default:
                                     break;
-                           }
-                       }
-
-                        // Check columns
-                        for (let i = 0; i < 3; i++) {
-                            let colString = '';
-                            for (let j = 0; j < 3; j++) {
-                                switch (GameBoard.getFromGrid(j, i)) {
-                                    case 'X':
-                                        colString += 'X';
-                                        break;
-                                    case 'O':
-                                        colString += 'O';
-                                        break;
-                                    default:
-                                        break;
-                                }
-                            }
-                            switch (colString) {
-                                case 'XXX':
-                                    winner = player1;
-                                    break;
-                                 case 'OOO':
-                                    winner = player2;
-                                    break;
-                                 default:
-                                     break;
-                            }
-                        }
-
-                        // Check diagonals
-                        let diagonalString1 = `${GameBoard.getFromGrid(0, 0)}${GameBoard.getFromGrid(1, 1)}${GameBoard.getFromGrid(2, 2)}`;
-                        let diagonalString2 = `${GameBoard.getFromGrid(2, 0)}${GameBoard.getFromGrid(1, 1)}${GameBoard.getFromGrid(0, 2)}`;
-                        if (diagonalString1 === 'XXX' || diagonalString2 === 'XXX') {
-                            winner = player1;
-                        } else if (diagonalString1 === 'OOO' || diagonalString2 === 'OOO') {
-                            winner = player2;
-                        } else {
-                            // pass
-                        }
-
-                        if (winner) {
-                            showGameResult(winner);
-                        }
-
-                        // If there is no winner and the game is over, then it's a tie
-                        if (!winner && playCounter === 9) {
-                            showGameResult('tie');
                         }
                     }
+                    switch (rowString) {
+                        case 'XXX':
+                            winner = player1;
+                            break;
+                            case 'OOO':
+                                winner = player2;
+                                break;
+                            default:
+                                break;
+                    }
                 }
-            })
-        )
-    })();
 
-});
+                    // Check columns
+                    for (let i = 0; i < 3; i++) {
+                        let colString = '';
+                        for (let j = 0; j < 3; j++) {
+                            switch (GameBoard.getFromGrid(j, i)) {
+                                case 'X':
+                                    colString += 'X';
+                                    break;
+                                case 'O':
+                                    colString += 'O';
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        switch (colString) {
+                            case 'XXX':
+                                winner = player1;
+                                break;
+                            case 'OOO':
+                                winner = player2;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
 
-/* 
+                    // Check diagonals
+                    let diagonalString1 = `${GameBoard.getFromGrid(0, 0)}${GameBoard.getFromGrid(1, 1)}${GameBoard.getFromGrid(2, 2)}`;
+                    let diagonalString2 = `${GameBoard.getFromGrid(2, 0)}${GameBoard.getFromGrid(1, 1)}${GameBoard.getFromGrid(0, 2)}`;
+                    if (diagonalString1 === 'XXX' || diagonalString2 === 'XXX') {
+                        winner = player1;
+                    } else if (diagonalString1 === 'OOO' || diagonalString2 === 'OOO') {
+                        winner = player2;
+                    } else {
+                        // pass
+                    }
 
-[0, 0] [0, 1] [0, 2]
-[1, 0] [1, 1] [1, 2]
-[2, 0] [2, 1] [2, 2]
+                    if (winner) {
+                        showGameResult(winner);
+                    }
 
-*/
+                    // If there is no winner and the game is over, then it's a tie
+                    if (!winner && playCounter === 9) {
+                        showGameResult('tie');
+                    }
+                }
+            }
+        })
+    )
+};   
